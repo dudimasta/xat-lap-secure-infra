6925b95566f5317dfcf1a739b844ccda04d0c058
Rafał Dudziński <rafal.dudzinski@xplusglobal.com>
2024-06-27 12:59:43 +0000

    c

--- infra/build.sh
+++ infra/build.sh
@@ -6,16 +6,15 @@ mkdir -p output
 rm ./output/*.json
 #
 # tworzymy bazowe zasoby w resource grupach (Azure Files, workflow i data bus)
-#    bicep build-params ./src/main.bicepparam --outfile ./output/main.json
-#    az deployment sub create --location $FILES_RG_LOCATION --template-file ./src/main.bicep --parameters ./output/main.json
-#    #
-#    # deploy maszyny z linuxem do testowania komunikacji
-#    bicep build-params ./src/testvm.bicepparam --outfile ./output/testvm.json
-#    az deployment group create -g $ACCESS_RG_NAME --template-file ./src/testvm.bicep --parameters ./output/testvm.json
+bicep build-params ./src/main.bicepparam --outfile ./output/main.json
+az deployment sub create --location $FILES_RG_LOCATION --template-file ./src/main.bicep --parameters ./output/main.json
 #
-#    # tworzymy private endpoint do bezpiecznej komunikacji od workflowów (LogicApps) do Az File
-#    bicep build-params ./src/private_endpoint_workflow.bicepparam --outfile ./output/private_endpoint_workflow.json
-#    az deployment sub create --location $FILES_RG_LOCATION --template-file ./src/private_endpoint_workflow.bicep --parameters ./output/private_endpoint_workflow.json
+# deploy maszyny z linuxem do testowania komunikacji
+bicep build-params ./src/testvm.bicepparam --outfile ./output/testvm.json
+az deployment group create -g $ACCESS_RG_NAME --template-file ./src/testvm.bicep --parameters ./output/testvm.json
+# tworzymy private endpoint do bezpiecznej komunikacji od workflowów (LogicApps) do Az File
+bicep build-params ./src/private_endpoint_workflow.bicepparam --outfile ./output/private_endpoint_workflow.json
+az deployment sub create --location $FILES_RG_LOCATION --template-file ./src/private_endpoint_workflow.bicep --parameters ./output/private_endpoint_workflow.json
 
 bicep build-params ./src/private_endpoint_dns.bicepparam --outfile ./output/private_endpoint_dns.json
 az deployment group create -g $WORKFLOW_RG_NAME --template-file ./src/private_endpoint_dns.bicep --parameters ./output/private_endpoint_dns.json
